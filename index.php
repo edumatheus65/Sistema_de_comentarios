d217328f3fd9822f05adb7d8cdbc993d70911e69
Eduardo Neves <ematheusneves13@gmail.com>
2022-08-31 13:28:43 -0300

    Criando formulario para envio de dados

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
-fild
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sistema de comentários</title>
+
+     <!-- Bootstrap -->
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.3/css/bootstrap.min.css" integrity="sha512-oc9+XSs1H243/FRN9Rw62Fn8EtxjEYWHXRvjS43YtueEewbS6ObfXcJNyohjHqVKFPoXXUxwc+q1K7Dee6vv9g==" crossorigin="anonymous"/>
+     
+</head>
+<body>
+    
+</body>
+</html>
+
+<fieldset>
+
+    <form action="" method="POST">
+
+        <label for="">Nome</label>
+        <input type="text">
+
+
+    </form>
+
+</fieldset>
